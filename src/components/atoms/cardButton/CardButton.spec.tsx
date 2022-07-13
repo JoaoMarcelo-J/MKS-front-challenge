aa34617149eb1d8e8258fb614b566a7471976f95
JoaoMarcelo-J <joaoniniho.twt@gmail.com>
2022-07-13 18:14:23 -0300

    fix: deploy

--- src/components/atoms/cardButton/CardButton.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { CardButton } from ".";
-import { product } from "./cardButtonMocks";
-
-test("card button renders correctly", () => {
-  const { debug } = render(<CardButton product={product} />);
-
-  debug();
-});
